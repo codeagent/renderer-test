bf16797a549f016aa9edcd730203ebb7375a4edd
codeagent <i.aleksey.yakovlev@gmail.com>
2022-06-21 12:06:22 +0200

    1 file modified, 1 added, 1 deleted

--- index.ts
+++ index.ts
@@ -5,7 +5,11 @@ import {
   Renderer,
   ArcRotationCameraController,
   bootstrap,
+  HttpClient,
+  HttpClientCache,
 } from '@cat3d/renderer';
+
+import GRAPH from './assets/scenes/scene.graph.xml';
 
 const canvas = document.getElementById('canvas') as HTMLCanvasElement;
 canvas.width = self.screen.width;
@@ -13,11 +17,20 @@ canvas.height = self.screen.height;
 
 (async () => {
   const injector = await bootstrap({ element: canvas });
-  const assetsManager = injector.resolve(AssetsManager);
-  const assetsCache = injector.resolve(AssetsCache);
+  const httpClient: HttpClient = injector.resolve(HttpClient);
+  httpClient.baseUrl =
+    'https://raw.githubusercontent.com/codeagent/renderer-test/master/assets/';
+
+  const assetsManager: AssetsManager = injector.resolve(AssetsManager);
+  const httpCache: HttpClientCache = injector.resolve(HttpClientCache);
+  httpCache.set(httpClient.baseUrl + 'scenes/scene.graph.xml', GRAPH);
+
   const sceneManager = injector.resolve(SceneManager);
   const renderer = injector.resolve(Renderer);
   const graph = await assetsManager.resolveSceneGraph('scenes/scene.graph.xml');
+  console.log(GRAPH);
+  console.log(graph);
+
   sceneManager.activeSceneGraph = graph;
   const camera = sceneManager.findById('main-camera');
 
