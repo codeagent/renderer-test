0791caa59f3a170f9c7091f21f4a8a14bd33eb0a
Alex <i.aleksey.yakovlev@gmail.com>
2022-06-21 11:33:31 +0200

    test

--- index.ts
+++ index.ts
@@ -1,1 +1,36 @@
-import { bootstrap } from '@cat3d/renderer';
+import {
+  AssetsManager,
+  AssetsCache,
+  SceneManager,
+  Renderer,
+  ArcRotationCameraController,
+  bootstrap,
+} from '@cat3d/renderer';
+
+const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+canvas.width = self.screen.width;
+canvas.height = self.screen.height;
+
+(async () => {
+  const injector = await bootstrap({ element: canvas });
+  const assetsManager = injector.resolve(AssetsManager);
+  const assetsCache = injector.resolve(AssetsCache);
+  const sceneManager = injector.resolve(SceneManager);
+  const renderer = injector.resolve(Renderer);
+  const graph = await assetsManager.resolveSceneGraph('scenes/scene.graph.xml');
+  sceneManager.activeSceneGraph = graph;
+  const camera = sceneManager.findById('main-camera');
+
+  const cameraController = new ArcRotationCameraController(camera);
+  let t = Date.now(),
+    dt = 0.0;
+
+  const draw = () => {
+    cameraController.update(dt);
+    renderer.render(camera);
+
+    (dt = (Date.now() - t) * 1.0e-3), (t = Date.now());
+    requestAnimationFrame(draw);
+  };
+  draw();
+})();
